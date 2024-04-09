5c211563936cea6b615a3f028078bed9f7548048
gabrielprojeto01 <62442346+gabrielprojeto01@users.noreply.github.com>
2024-04-10 00:20:00 +0100

    Update web.php

--- PeA/routes/web.php
+++ PeA/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OwnerController;
+use App\Http\Controllers\Api\ApiController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +18,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users' ,[OwnerController::class ,'index'])->name('users.store');
+Route::get('/users' ,[ApiController::class ,'index'])->name('users.store');
+Route::post('/create' ,[ApiController::class ,'register'])->name('user.register');
+Route::delete('/users/{user}' , [ApiController::class , 'destroy'])->name("user.destroy");
+
+Route::get('/usersform',function(){
+    return view('usersform');
+});
+
+Route::get('/policesform',function(){
+    return view('policesform');
+});
+
+Route::get('/stationsform',function(){
+    return view('stationsform');
+});
+
+Route::get('/chooseaccounttype',function(){
+    return view('chooseaccounttype');
+});
 
 
