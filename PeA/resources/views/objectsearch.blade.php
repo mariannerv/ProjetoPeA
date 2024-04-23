b0daf2f9918fb0581553b3eadbfd6fcda61ea29f
dramataparty <101320231+dramataparty@users.noreply.github.com>
2024-04-23 17:21:06 +0100

    minor update

--- PeA/resources/views/objectsearch.blade.php
+++ PeA/resources/views/objectsearch.blade.php
@@ -22,9 +22,9 @@
     <!-- Results will be displayed here -->
   </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <!-- Bootstrap JS (optional, only if you need JavaScript features) -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
