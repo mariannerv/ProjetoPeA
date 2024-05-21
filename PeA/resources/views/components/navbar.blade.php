6599188a35c3eedb3af693e653e87508b31e55e7
dramataparty <101320231+dramataparty@users.noreply.github.com>
2024-05-21 14:24:24 +0100

    notif beginnings

--- PeA/resources/views/components/navbar.blade.php
+++ PeA/resources/views/components/navbar.blade.php
@@ -19,6 +19,22 @@
                   Home
                 </a>
         </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2z"/>
+                <path d="M8 1a5 5 0 0 1 5 5v3.993l.764 2.543a1 1 0 0 1-.97 1.321H2.206a1 1 0 0 1-.97-1.321L2 9.993V6a5 5 0 0 1 5-5zm0 1a4 4 0 0 0-4 4v3.8l-.682 2.27a.5.5 0 0 0 .485.646h9.394a.5.5 0 0 0 .485-.646L12 9.8V6a4 4 0 0 0-4-4z"/>
+              </svg>
+              <span class="badge bg-danger">3</span> <!-- Example badge for 3 unread notifications -->
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
+              <li><a class="dropdown-item" href="#">Notification 1</a></li>
+              <li><a class="dropdown-item" href="#">Notification 2</a></li>
+              <li><a class="dropdown-item" href="#">Notification 3</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">View all notifications</a></li>
+            </ul>
+          </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
