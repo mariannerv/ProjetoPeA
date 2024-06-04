a11f7627d445220235910c3c82b8a079318cbbd1
gabrielprojeto01 <62442346+gabrielprojeto01@users.noreply.github.com>
2024-06-05 00:40:46 +0100

    rrr

--- PeA/resources/views/objects/foundobjectedit.blade.php
+++ PeA/resources/views/objects/foundobjectedit.blade.php
@@ -12,7 +12,7 @@ if (!Auth::guard('police')->check()) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-    <title>Registar Objeto Perdido</title>
+    <title>Editar Objeto Perdido</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
@@ -72,7 +72,7 @@ if (!Auth::guard('police')->check()) {
                                     </div>
                                     <input type="hidden" name="policeStationId" value="{{ Auth::guard('police')->user()->policeStationId }}">
                                     <div class="col-12">
-                                        <button class="btn btn-primary" type="submit">Confirmar alterações</button>
+                                        <button class="btn btn-primary" type="submit">Editar</button>
                                         <button class="btn btn-secondary" onclick="goBack()">Cancelar</button>
                                     </div>
                                 </form>
