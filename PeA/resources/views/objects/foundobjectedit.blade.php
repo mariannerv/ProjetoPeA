18e6fff4717352598c79146edcf91af3627396fa
gabrielprojeto01 <62442346+gabrielprojeto01@users.noreply.github.com>
2024-06-05 00:20:28 +0100

    Update foundobjectedit.blade.php

--- PeA/resources/views/objects/foundobjectedit.blade.php
+++ PeA/resources/views/objects/foundobjectedit.blade.php
@@ -12,7 +12,7 @@ if (!Auth::guard('police')->check()) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-    <title>Registar Objeto Perdido</title>
+    <title>Editar Objeto Perdido</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
